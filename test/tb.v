1e87659f9c01b106a5d51744cd69c8033a19eeea
Toivo Henningsson <toivo.h.h@gmail.com>
2024-09-28 08:00:33 +0200

    Use TT top module in tb.v

--- test/tb.v
+++ test/tb.v
@@ -18,18 +18,29 @@ module tb();
 
 
 	reg clk;
-	reg rst_n = 0;
+	reg rst_n;
 	wire reset = !rst_n;
 
 
 	wire [IO_BITS-1:0] rx_pins;
 	wire [IO_BITS-1:0] tx_pins;
 
-
+/*
 	julia_top top(
 		.clk(clk), .reset(reset),
 		.rx_pins(rx_pins), .tx_pins(tx_pins)
 	);
+	*/
+
+	wire [7:0] uo_out, uio_in, uio_out;
+	tt_um_toivoh_pio_ram_emu_example top(
+		.clk(clk), .rst_n(rst_n),
+		.uo_out(uo_out), .uio_in(uio_in), .uio_out(uio_out)
+	);
+	assign tx_pins = uio_out[5:4];
+	assign uio_in[5:0] = 0;
+	assign uio_in[7:6] = rx_pins;
+
 
 	// Gives a 22 cycle latency from start bit of read message to start bit of first reply, given that tx_pins and rx_pins are registered in the design
 	pio_ram_emulator_model #(.READ_LATENCY(22), .ERROR_RESPONSE(1)) ram_emu(
