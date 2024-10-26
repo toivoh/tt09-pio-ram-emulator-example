49138980a3e00d32d13e5b10ad0802ee90978f0b
Toivo Henningsson <toivo.h.h@gmail.com>
2024-10-26 09:31:55 +0200

    Fix GL test, make RTL/GL test check emulator RAM contents

--- test/tb.v
+++ test/tb.v
@@ -4,51 +4,77 @@
  */
 
 `default_nettype none
-`timescale 1ns/1ps
+`timescale 1ns / 1ps
 
-module tb();
+/* This testbench just instantiates the module and makes some convenient wires
+   that can be driven / tested by the cocotb test.py.
+*/
+module tb ();
 
+	// Dump the signals to a VCD file. You can view it with gtkwave.
 	initial begin
-		$dumpfile ("tb.vcd");
-		$dumpvars (0, tb);
+		$dumpfile("tb.vcd");
+		$dumpvars(0, tb);
 		#1;
 	end
 
-	localparam IO_BITS = 2;
-
-
+	// Wire up the inputs and outputs:
 	reg clk;
 	reg rst_n;
-	wire reset = !rst_n;
+	reg ena;
+	reg [7:0] ui_in;
+	reg [7:0] uio_in;
+	wire [7:0] uo_out;
+	wire [7:0] uio_out;
+	wire [7:0] uio_oe;
+`ifdef GL_TEST
+	wire VPWR = 1'b1;
+	wire VGND = 1'b0;
+`endif
 
 
-	wire [IO_BITS-1:0] rx_pins;
+	// Connect TX and RX pins to the design under test
+	localparam IO_BITS = 2;
+
 	wire [IO_BITS-1:0] tx_pins;
+	wire [IO_BITS-1:0] rx_pins;
 
-/*
-	julia_top top(
-		.clk(clk), .reset(reset),
-		.rx_pins(rx_pins), .tx_pins(tx_pins)
+	assign tx_pins = uio_out[5:4]; // Change if your design uses other pins for tx_pins. Note that they have to be consecutive to the RP2040!
+
+	// Replace uio_in[7:6] with rx_pins coming from the RAM emulator model.
+	// Change if your design uses other pins for rx_pins. Note that they have to be consecutive to the RP2040!
+	wire [7:0] uio_in_actual;
+	assign uio_in_actual[5:0] = uio_in[5:0];
+	assign uio_in_actual[7:6] = rx_pins;
+
+
+	// Replace tt_um_example with your module name:
+	tt_um_toivoh_pio_ram_emu_example user_project (
+
+		// Include power ports for the Gate Level test:
+`ifdef GL_TEST
+		.VPWR(VPWR),
+		.VGND(VGND),
+`endif
+
+		.ui_in  (ui_in),    // Dedicated inputs
+		.uo_out (uo_out),   // Dedicated outputs
+		.uio_in (uio_in_actual),   // IOs: Input path. uio_in_actual[7:6] have been hardwired to rx_pins coming from the RAM emulator model.
+		.uio_out(uio_out),  // IOs: Output path
+		.uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
+		.ena    (ena),      // enable - goes high when design is selected
+		.clk    (clk),      // clock
+		.rst_n  (rst_n)     // not reset
 	);
-	*/
 
-	wire [7:0] uo_out, uio_in, uio_out;
-	tt_um_toivoh_pio_ram_emu_example top(
-		.clk(clk), .rst_n(rst_n),
-		.uo_out(uo_out), .uio_in(uio_in), .uio_out(uio_out)
-	);
-	assign tx_pins = uio_out[5:4];
-	assign uio_in[5:0] = 0;
-	assign uio_in[7:6] = rx_pins;
-
-
+	// Instantiate RAM emulator model
 	// Gives a 22 cycle latency from start bit of read message to start bit of first reply, given that tx_pins and rx_pins are registered in the design
 	pio_ram_emulator_model #(.READ_LATENCY(22), .ERROR_RESPONSE(1)) ram_emu(
-		.clk(clk), .reset(reset),
+		.clk(clk), .reset(!rst_n),
 		.rx_pins(tx_pins), .tx_pins(rx_pins)
 	);
 
-
+	// Running counter to make it easier to inspect the vcd file
 	int running_counter = 0;
 	always @(posedge clk) running_counter <= running_counter + 1;
 endmodule
--- test/test.py
+++ test/test.py
@@ -7,24 +7,45 @@ from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 
 
 @cocotb.test()
-async def test(dut):
-	dut._log.info("start")
-	clock = Clock(dut.clk, 2, units="us")
+async def test_project(dut):
+	dut._log.info("Start")
+
+	clock = Clock(dut.clk, 10, units="us")
 	cocotb.start_soon(clock.start())
 
-	top = dut.top
+	# Initialize part of the RAM emulator's RAM.
 	ram_emu = dut.ram_emu
 	ram = ram_emu.RAM
-
 	for i in range(256):
-		value = 0x0100 + 0x0202*(i & 7) + 0x1010*((~i >> 3) & 15)
+		#value = 0x0100 + 0x0202*(i & 7) + 0x1010*((~i >> 3) & 15)
+		value = 0 # RAM is initialized to zero by the RAM emulator, unless customized
 		ram[i].value = value
 		#print(i, ":", hex(value), ",", end="")
 
-	# reset
-	dut._log.info("reset")
+	# Reset
+	dut._log.info("Reset")
+	dut.ena.value = 1
+	dut.ui_in.value = 0
+	dut.uio_in.value = 0xff
 	dut.rst_n.value = 0
 	await ClockCycles(dut.clk, 10)
 	dut.rst_n.value = 1
 
-	await ClockCycles(dut.clk, 800*2)
+	await ClockCycles(dut.clk, (800*5+1)*2)
+
+
+	# Print RAM values that have been updated
+	last_addr = ram_emu.write_addr.value.integer - 1 # The write address gets incremented by one by the RAM emulator after a successful write, compensate
+
+	print("initialized_ram = [", end="")
+	for i in range(last_addr):
+		print(ram[i].value.integer, end=", ")
+	print("]")
+
+	# Compare against expected result,
+	# taken from earlier printout
+	initialized_ram = [0, 0, 0, 0, 0, 0, 0, 0, 16384, 21845, 21845, 21845, 21845, 21845, 21845, 21845, 21845, 21845, 21845, 21845, 21845, 21845, 21845, 21845, 21845, 21845, 21845, 21845, 21845, 21845, 21845, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 21504, 21845, 21845, 21845, 21845, 21845, 21845, 21845, 21845, 21845, 21845, 21845, 21845, 21845, 21845, 21845, 21845, 21845, 21845, 21845, 21845, 21845, 21845, 85, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 21840, 21845, 21845, 21845, 21845, 21845, 21845, ]
+
+	assert last_addr == len(initialized_ram)
+	for (i, data) in enumerate(initialized_ram):
+		assert ram[i].value.integer == data
